7dce6154dd7e05adf532cea6085ff8ec2420f5f6
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-08-14 10:40:34 -0300

    mudança dnv

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post("/api/add-animal", (req, res) => {
 		caractGeral,
 		peconhento,
 		agressivo,
-	} = req.body.animalData;
+	} = req.body;
 	const imagem = req.file ? req.file.filename : null;
 
 	if (!nomePopular) {
